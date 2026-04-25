e02b233f443fb4731b8de4eedb89778bbd3a21bb
고오고오 <tjddnjsqls@gmail.com>
2026-04-25 19:47:37 +0900

    member test commit2

--- src/main/java/org/example/Main.java
+++ src/main/java/org/example/Main.java
@@ -4,6 +4,6 @@ package org.example;
 // 에디터 여백에 있는 <icon src="AllIcons.Actions.Execute"/> 아이콘을 클릭하세요.
 public class Main {
     public static void main(String[] args) {
-        System.out.println("feature_test1");
+        System.out.println("member test commit2");
     }
 }
