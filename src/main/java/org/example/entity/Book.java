5575632f3f35bf47abbdc7be0ef71cf107624a2b
성원빈 <tjddnsjqls@gmail.com>
2026-05-06 22:43:51 +0900

    [성원빈] 의존성 문제 해결

--- src/main/java/org/example/entity/Book.java
+++ src/main/java/org/example/entity/Book.java
@@ -1,11 +1,7 @@
 package org.example.entity;
 
 import lombok.*;
-import jakarta.persistence.Entity;
-import jakarta.persistence.Id;
-import jakarta.persistence.GeneratedValue;
-import jakarta.persistence.GenerationType;
-import jakarta.persistence.Column;
+import jakarta.persistence.*;
 
 @Getter
 @Setter
